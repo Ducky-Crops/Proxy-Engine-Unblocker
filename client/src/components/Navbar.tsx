6efcadcacffb956981766b766c7e57df67f8d4e2
catttttttt <ameertaimoorahmad@gmail.com>
2026-02-28 21:25:45 -0500

    yes

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import '../App.css';
+
+export function Credits_Label() {
+  return (
+    <navbar>
+      <ul>
+        <li><a href=''>Home</a></li>
+      </ul>
+    </navbar>
+  );
+}
